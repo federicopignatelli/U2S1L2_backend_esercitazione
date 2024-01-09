ec35f2ddd5f60f95ffcf8baa641364412b208778
federicopignatelli <federicopignatelli@icloud.com>
2024-01-09 16:13:11 +0100

    Initial commit

---
+++ src/main/java/federicopignatelli/U2S1L2_backend_esercitazione/U2S1L2BackendEsercitazioneApplication.java
@@ -0,0 +1,13 @@
+package federicopignatelli.U2S1L2_backend_esercitazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2S1L2BackendEsercitazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2S1L2BackendEsercitazioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/federicopignatelli/U2S1L2_backend_esercitazione/U2S1L2BackendEsercitazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package federicopignatelli.U2S1L2_backend_esercitazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2S1L2BackendEsercitazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
